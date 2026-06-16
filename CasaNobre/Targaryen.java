80969d520a87ea0806eea7cf49df7645b47fbaab
faloni <40303923+faloni1@users.noreply.github.com>
2026-06-16 20:13:54 -0300

    Create Targaryen.java

---
+++ CasaNobre/Targaryen.java
@@ -0,0 +1,27 @@
+package CasaNobre;
+
+public class Targaryen extends CasaNobre {
+    protected boolean imunidadeAoFogo;
+
+    public Targaryen(String nome, String lema, Reino reino, boolean imunidadeAoFogo) {
+        super(nome, lema, reino);
+        this.imunidadeAoFogo = imunidadeAoFogo;
+    }
+
+    @Override
+    public void convocarVassalos() {
+        if (this.imunidadeAoFogo) {
+            System.out.println("[" + this.nome + " - Casa Targaryen]: " + this.lema + "! Com o poder dos dragões, nossos vassalos marcham sem hesitação. Ninguém ousará resistir!");
+        } else {
+            System.out.println("[" + this.nome + " - Casa Targaryen]: Os vassalos da casa são convocados, mas sem o sangue do dragão a autoridade é questionada...");
+        }
+    }
+
+    public void vincularDragao() {
+        if (this.imunidadeAoFogo) {
+            System.out.println("[" + this.nome + " - Casa Targaryen]: O vínculo com o dragão é estabelecido! As chamas não podem nos tocar. Dracarys!");
+        } else {
+            System.out.println("[" + this.nome + " - Casa Targaryen]: Sem imunidade ao fogo, a tentativa de vincular o dragão é perigosa. O dragão rejeita o vínculo...");
+        }
+    }
+}
