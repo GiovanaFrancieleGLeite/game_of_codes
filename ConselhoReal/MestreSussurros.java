fa4128e815565123b7b5a89022bb01ff33161aae
David Barbon <davidfilhobarbon@gmail.com>
2026-06-16 12:34:16 -0300

    Corrigindo bugs

--- ConselhoReal/MestreSussurros.java
+++ ConselhoReal/MestreSussurros.java
@@ -2,20 +2,19 @@ package ConselhoReal;
 
 public class MestreSussurros extends ConselhoReal{
 
-    public MestreSussurros(String nome, int nivelLealdade) {
-        this.nome = nome;
-        this.lealdade = nivelLealdade;
+    public MestreSussurros(String nome, int lealdade, Reino reino) {
+        super(nome, lealdade, reino);
     }
 
     @Override
     public void ativar() {
 
         if (this.lealdade > 75) {
-            System.out.println("[" + this.nome + " - Mestre dos Sussurros]: Meus passarinhos me trouxeram a verdade, Majestade, a casa Lannister está movimentando tropas secretas na fronteira!");
+            System.out.println("[" + this.nomeCons + " - Mestre dos Sussurros]: Meus passarinhos me trouxeram a verdade, Majestade, a casa Lannister está movimentando tropas secretas na fronteira!");
         } else if (this.lealdade>= 45 && this.lealdade <= 75) {
-            System.out.println("[" + this.nome + " - Mestre dos Sussurros]: 'Há rumores nas tavernas de que os Greyjoy estão construindo novos navios, mas ainda estou investigando a fundo.'");
+            System.out.println("[" + this.nomeCons + " - Mestre dos Sussurros]: 'Há rumores nas tavernas de que os Greyjoy estão construindo novos navios, mas ainda estou investigando a fundo.'");
         } else {
-            System.out.println("[" + this.nome + " - Mestre dos Sussurros]: 'Majestade, meus espiões garantem que o Norte jurou fidelidade eterna ao senhor. Pode desarmar suas fronteiras sem medo");
+            System.out.println("[" + this.nomeCons + " - Mestre dos Sussurros]: 'Majestade, meus espiões garantem que o Norte jurou fidelidade eterna ao senhor. Pode desarmar suas fronteiras sem medo");
         }
     }
 }
