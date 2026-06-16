021ffa01c5c1aafd0c9953b8411c716539b2a938
David Barbon <davidfilhobarbon@gmail.com>
2026-06-16 11:55:44 -0300

    Update Navio.java

--- Militar/Navio.java
+++ Militar/Navio.java
@@ -1,12 +1,12 @@
 package Militar;
 
-public class Navio extends Militar {
+public class Navio extends Militar{
     protected String nomeNav;
     protected float peso;
     protected int tripulacao;
 
-    public Navio(Reino reino, int poder, String nomeNav, float peso, int tripulacao){
-        super(poder, reino);
+    public Navio(Reino reino, int poder, String nomeNav, float peso, int tripulacao, double custo){
+        super(poder, reino, custo);
         this.nomeNav = nomeNav;
         this.peso = peso;
         this.tripulacao = tripulacao;
@@ -19,13 +19,14 @@ public class Navio extends Militar {
 
     public void mostrarInfo(){
         System.out.println("=== INFORMAÇÕES DO NAVIO === ");
-        System.out.println("Reino.Reino Proprietário: " + reino.getNome());
-        System.out.println("Nome do Militar.Navio: " + nomeNav);
-        System.out.println("Peso do Militar.Navio: " + peso);
-        System.out.println("Tripulação do Militar.Navio: " + tripulacao);
+        System.out.println("Reino Proprietário: " + reino.getNome());
+        System.out.println("Nome do Navio: " + nomeNav);
+        System.out.println("Peso do Navio: " + peso);
+        System.out.println("Tripulação do Navio: " + tripulacao);
     }
 
     public void navegar(){
         System.out.println("O navio " + nomeNav + " está navegando!");
     }
 }
+
