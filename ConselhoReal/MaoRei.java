8a193accfe1d1134117e69dc2763cfc09ddc46be
David Barbon <davidfilhobarbon@gmail.com>
2026-06-16 12:26:55 -0300

    Corrigindo bugs

--- ConselhoReal/MaoRei.java
+++ ConselhoReal/MaoRei.java
@@ -1,20 +1,18 @@
 package ConselhoReal;
 
 public class MaoRei extends ConselhoReal{
-
-    public MaoRei(String nome, int nivelLealdade){
-        this.nome = nome;
-        this.lealdade = nivelLealdade;
+    public MaoRei(String nome, int lealdade, Reino reino){
+        super(nome, lealdade, reino);
     }
 
     @Override
-    public ativar(){
+    public void ativar(){
         if(this.lealdade > 75){
-            System.out.println("["+ this.nome + " - Mão do Rei]: Majestade, como sua Mão, recomendo reforçar as patrulhas e poupar ouro. O inverno está se aproximando e o reino precisa de estabilidade.");
+            System.out.println("["+ this.nomeCons + " - Mão do Rei]: Majestade, como sua Mão, recomendo reforçar as patrulhas e poupar ouro. O inverno está se aproximando e o reino precisa de estabilidade.");
         } else if (this.lealdade > 40 && this.lealdade <= 75) {
-            System.out.println("["+ this.nome + " - Mão do Rei]: Majestade, os lords estão inquietos. Recomendo diplomacia e realizar um banquete.");
+            System.out.println("["+ this.nomeCons + " - Mão do Rei]: Majestade, os lordes estão inquietos. Recomendo diplomacia e realizar um banquete.");
         }else{
-            System.out.println("["+ this.nome + " - Mão do Rei]: Majestade, não precisa se preucupar, o reino nunca esteve tão seguro");
+            System.out.println("["+ this.nomeCons + " - Mão do Rei]: Majestade, não precisa se preocupar, o reino nunca esteve tão seguro");
         }
     }
 }
