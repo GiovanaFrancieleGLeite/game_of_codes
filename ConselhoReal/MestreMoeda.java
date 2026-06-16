e1c9b68b074d67e315f125306703fbb65aa78bcb
David Barbon <davidfilhobarbon@gmail.com>
2026-06-16 12:33:14 -0300

    Corrigindo bugs

--- ConselhoReal/MestreMoeda.java
+++ ConselhoReal/MestreMoeda.java
@@ -1,25 +1,24 @@
 package ConselhoReal;
 
 public class MestreMoeda extends ConselhoReal{
-    private double ouroArrecadado;
+    protected double ouroArrecadado;
 
-    public MestreMoeda(String nome, int nivelLealdade) {
-        this.nome = nome;
-        this.lealdade = nivelLealdade;
-        this.ouroArrecadado = 1000.0;
+    public MestreMoeda(String nome, int lealdade, Reino reino, double ouroArrecadado) {
+        super(nome, lealdade, reino);
+        this.ouroArrecadado = ouroArrecadado;
     }
 
     @Override
     public void ativar() {
         if (this.lealdade > 70) {
-            this.ouroArrecadado = 500.0;
-            System.out.println("[" + this.nome + " - Mestre da Moeda]: Majestade, a população pagou os impostos em dia. Arrecadamos " + this.ouroArrecadado + " moedas.");
+            this.ouroArrecadado += 500.0;
+            System.out.println("[" + this.nomeCons + " - Mestre da Moeda]: Majestade, a população pagou os impostos em dia. Arrecadamos " + this.ouroArrecadado + " moedas.");
         } else if (this.lealdade >= 40 && this.lealdade<= 70) {
-            this.ouroArrecadado = 300.0;
-            System.out.println("[" + this.nome + " - Mestre da Moeda]: Os cobradores voltaram das vilas. Arrecadamos " + this.ouroArrecadado + " moedas.'");
+            this.ouroArrecadado += 300.0;
+            System.out.println("[" + this.nomeCons + " - Mestre da Moeda]: Os cobradores voltaram das vilas. Arrecadamos " + this.ouroArrecadado + " moedas.'");
         } else {
-            this.ouroArrecadado = 100.0;
-            System.out.println("[" + this.nome + " - Mestre da Moeda]: O povo está rebelde e quase ninguém pagou. Só consegui pegar " + this.ouroArrecadado + " moedas. ");
+            this.ouroArrecadado += 100.0;
+            System.out.println("[" + this.nomeCons + " - Mestre da Moeda]: O povo está rebelde e quase ninguém pagou. Só consegui pegar " + this.ouroArrecadado + " moedas. ");
         }
     }
 
@@ -29,10 +28,10 @@ public class MestreMoeda extends ConselhoReal{
         if (this.ouroArrecadado >= custoMilitar) {
             this.ouroArrecadado -= custoMilitar;
             System.out.println("As tropas foram pagas. Sobrou: " + this.ouroArrecadado + " moedas.");
-        }
-
-        else {
+            return true;
+        } else {
             System.out.println("As tropas não foram pagas! Falta ouro para pagar os soldados.");
+            return false;
         }
     }
 }
